96fc732f00ad7fe8ef741bf0ea886b421f2d1514
namo <nnnnnnnn+pakhomenkov@mail.ru.noreply.github.com>
2025-06-25 19:44:06 +0300

    соптимизировал .gitignore

--- sh/require.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo Press Ctrl-C if unsure!
-read
-pip3 install -r requirements.txt
--- sh/softice.sh
+++
@@ -1,3 +0,0 @@
-cd /home/app/Projects/-softice/
-/media/data/Projects/pyenv/pytelegram/bin/python softice.py
-read
