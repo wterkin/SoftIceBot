d1a5f5c1877aa15d25220a85dda93b94b67d0cc4
Andrey Pakhomenkov <pakhomenkov.ap@gmail.com>
2025-09-30 17:44:54 +0300

    Тесты метеоролога - продолжение

--- tests/test_meteorolog.py
+++ tests/test_meteorolog.py
@@ -23,8 +23,27 @@ class CTestMeterolog(TestCase):
     
     def test_parse_weather(self):
         
-        data: list = []
-        item: dict = {}
-        item["dt"] = dtime.now.timestamp
-
-
+        data: dict = {}
+        temp_list: list = [10, 11, 12, 13]
+        press_list: list = [700, 701, 702, 703]
+        hum_list: list = [70, 80, 90, 100]
+        wind_speed_list: list = [10, 20, 30, 40]
+        wind_deg_list: list = [0, 45, 90, 180]
+        data["list"] = []
+        #now: dtime.datetime = dtime.now()
+        now: dtime.datetime = dtime.datetime.now()
+        
+        for idx in range(0, len(temp_list)):
+            
+            item: dict = {}
+            item["dt"] = now.timestamp()
+            item['main']: dict = {}
+            item['main']["temp"] = temp_list[idx]
+            item['main']["pressure"] = press_list[idx]
+            item['main']["humidity"] = hum_list[idx]
+            item['wind']: dict = {}
+            item["wind"]["speed"] = wind_speed_list[idx]
+            item["wind"]["deg"] = wind_deg_list[idx]
+            data["list"].append(item)
+        result: str = "Темп.: 100 - 0 °C,  давл.: 7500 - 0 мм.рт.ст.,  влажн.: 100 - 0 %,  ветер: 200 м/с сев.  - 0 м/c сев. , "    
+        self.assertEqual(meteorolog.parse_weather(data, now), result)
